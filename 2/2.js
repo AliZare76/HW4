4c1b1c64d07bb0f6db042ff1d4f37fe22a836f27
AliZare76 <61268140+AliZare76@users.noreply.github.com>
2020-04-02 16:32:03 +0430

    Create 2.js

---
+++ 2/2.js
@@ -0,0 +1,8 @@
+let diffrence = function (firstDate, secondeDate) {
+
+    const year = +firstDate.getFullYear() - +secondeDate.getFullYear()
+    const month = +firstDate.getMonth() - +secondeDate.getMonth()
+    const day = +firstDate.getDate() - +secondeDate.getDate()
+
+    return `Year: ${year} Month: ${month} day: ${day}`
+}
